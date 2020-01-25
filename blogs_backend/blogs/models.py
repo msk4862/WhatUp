dab7b7f684bf1f7aa3b9f5a649eb882942711636
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-01-25 19:38:22 +0530

    initial commit

---
+++ blogs_backend/blogs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)    # auto_now_add=True makes date equals to
+                                                                #  date of creation(fixed value)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # if user is deleted than all of his posts will be deletd
