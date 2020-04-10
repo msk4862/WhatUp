9d332eea054f3d0b57de166fe247fa80bbb2194e
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-04-10 13:31:12 +0530

    backend initial

--- blogs_backend/blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ListPosts.as_view()),
-    path('<int:pk>/', views.DetailPost.as_view())
-]
--- blogs_backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import UserCreateAPIView, UserLoginAPIView
-
-urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('login/', UserLoginAPIView.as_view(), name='login'),
-]
