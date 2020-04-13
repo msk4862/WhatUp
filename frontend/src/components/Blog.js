61dfd6dd9a96a610869bc9402dada9db184a6f7f
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-04-14 01:10:42 +0530

    styled updated

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 import './styles/Blog.css'
+
 function Blog(props) {
 
     return (
-        <div className='row card'>
-            <div className='col-10 col--sm-10 card-body'>
-                <h4 className='card-title'>{props.title}</h4>
+        <div className='card'>
+            <div className='card-body'>
+                <h4 className='card-title'><a href='#'>{props.title}</a></h4>
                 <p className='card-text'>{props.desc}</p>
                 <div className='row justify-content-between'>
-                    <p className='col-sm-3'>Created on {props.date}</p>
-                    <div className='col-4 col-sm-4 col-offset-last'>
+                    <p className='col-6 col-sm-6 date'>Created on {props.date}</p>                    
+                    <div className='col-4 col-sm-4 ml-auto'>
                         <i><span>by</span>
                             <a href='#' className='card-link'> author</a>
                         </i>
---
+++ frontend/src/components/BlogPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogPage (props) {
+    return (
+        <div>This is blog body</div>
+    )
+}
+
+export default BlogPage
