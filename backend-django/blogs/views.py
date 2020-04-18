ef7fa69a19ea7aabe4d1b1b7a5d7437f7e8a88ab
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-04-19 00:54:29 +0530

    api view fix

--- backend-django/blogs/views.py
+++ backend-django/blogs/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Blog
-from .serializers import *
+from .serializers import BlogSerializer
+
+
+class BlogListApiView(generics.ListCreateAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+
 
 class BlogDetailApiView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = Blog
+    serializer_class = BlogSerializer
     queryset = Blog.objects.all()
