57983cbd324fdf8e210c95e4281a4781f39b0b59
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-04-19 21:15:16 +0530

    user display style updated

--- frontend/src/components/Blogs/Blog.js
+++ frontend/src/components/Blogs/Blog.js
@@ -10,7 +10,7 @@ const Blog = (props) => {
             <div className='card-body'>
                 <h4 className='card-title'><Link to={`/blogs/${props.id}`}>{props.title}</Link></h4>
                 <p className='card-text'>{props.desc}</p>
-                <div className='row justify-content-between align-items-center'>
+                <div className='row meta-date justify-content-between align-items-center'>
                     <p className='col-6 col-sm-6 date'>Created on {props.date}</p>                    
                     <div className='col-4 col-sm-4 ml-auto'>
                         <i><span>by </span>
