e534ed5951988f4cfe1f4035919aa3797d05b853
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-09 01:10:27 +0530

    prop deconstruction

--- frontend/src/components/Blogs/Blog.js
+++ frontend/src/components/Blogs/Blog.js
@@ -2,22 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import '../styles/Blogs/Blog.css'
+import UserHeader from '../UserHeader'
 
-const Blog = (props) => {
+const Blog = ({id, title, desc, date, author}) => {
 
     return (
         <div className='card'>
             <div className='card-body'>
-                <h4 className='card-title'><Link to={`/blogs/${props.id}`}>{props.title}</Link></h4>
-                <p className='card-text'>{props.desc}</p>
+                <h4 className='card-title'><Link to={`/blogs/${id}`}>{title}</Link></h4>
+                <p className='card-text'>{desc}</p>
                 <div className='row meta-date justify-content-between align-items-center'>
-                    <p className='col-6 col-sm-6 date'>Created on {props.date}</p>                    
+                    <p className='col-6 col-sm-6 date'>Created on {date}</p>                    
                     <div className='col-4 col-sm-4 ml-auto'>
                         <i><span>by </span>
                             <a href='#' className='card-link'>
-                                <strong>
-                                    {props.firstname} {props.lastname}
-                                </strong> 
+                                <UserHeader author_id={author}/>
                             </a>
                         </i>
                     </div>
