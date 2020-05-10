b1b4bfd6c5418d1d31d87046e5d684054fda587f
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-10 16:47:19 +0530

    blog model fix

--- backend-django/blogs/models.py
+++ backend-django/blogs/models.py
@@ -9,7 +9,7 @@ class Blog(models.Model):
     BodyMeta                = models.CharField(max_length=500, blank=True, null=True)
     Body                    = models.TextField()
     DateCreated             = models.DateField(auto_now=True)
-    Author                  = models.OneToOneField('user.Author', on_delete=models.CASCADE, related_name='author')
+    Author                  = models.ForeignKey('user.Author', on_delete=models.CASCADE, related_name='author')
 
     def __str__(self):
         return self.Title
