82c9d4091c2c42a9b57b1ac98670e357122a6907
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-10 16:53:09 +0530

    myblogs implemented

--- frontend/src/components/Blogs/BlogList.js
+++ frontend/src/components/Blogs/BlogList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import {connect} from 'react-redux'
+import jwt_decode from 'jwt-decode'
 
 import { fetchBlogs, login } from '../../actions'
 import Blog from './Blog'
@@ -14,22 +15,43 @@ const BlogList = (props) => {
             // trying login using browser cache
             props.login()
         }
+
     }, [])
+
 
     function renderBlogs() {
         if(props.blogs) {
-            return props.blogs.map (blog => {
-                    return <Blog 
-                                key={blog.id} 
-                                id={blog.id}
-                                title={blog.Title} 
-                                desc={blog.BodyMeta} 
-                                date={blog.DateCreated}
-                                author={blog.Author}
-                            />
-                }
-            )
-        } else {
+            if( props.location.pathname === "/blogs/my-blogs" & props.auth.isLoggedIn) {
+                // render current user's blog only
+                const user_id = jwt_decode(props.auth.token).user_id
+
+                return props.blogs.map (blog => {
+                    if (blog.Author === user_id) {
+                        return <Blog 
+                                    key={blog.id} 
+                                    id={blog.id}
+                                    title={blog.Title} 
+                                    desc={blog.BodyMeta} 
+                                    date={blog.DateCreated}
+                                    author={blog.Author}
+                                />
+                    }
+                })
+            }
+            else {
+                return props.blogs.map (blog => {
+                        return <Blog 
+                                    key={blog.id} 
+                                    id={blog.id}
+                                    title={blog.Title} 
+                                    desc={blog.BodyMeta} 
+                                    date={blog.DateCreated}
+                                    author={blog.Author}
+                                />
+                    })
+            }
+        }
+        else {
             return <h2>Sorry! No blogs yet written.</h2>
         }
     }
