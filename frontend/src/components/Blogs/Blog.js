c7d8ee5e5676d8ae7a18d7a9dc7aa5814f030886
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-10 16:52:48 +0530

    style fix

--- frontend/src/components/Blogs/Blog.js
+++ frontend/src/components/Blogs/Blog.js
@@ -29,9 +29,9 @@ const Blog = ({id, title, desc, date, author, auth}) => {
             <div className='card-body'>
                 <h4 className='card-title'><Link to={`/blogs/${id}`}>{title}</Link></h4>
                 <p className='card-text'>{desc}</p>
-                <div className='row meta-date justify-content-between align-items-center'>
-                    <p className='col-6 col-sm-6 date'>Created on {date}</p>                    
-                    <div className='col-6 col-sm-6 ml-auto'>
+                <div className='row meta-data justify-content-between align-items-center'>
+                    <p className='col-6 col-sm-6'>Created on {date}</p>                    
+                    <div className='col-6 col-sm-6'>
                         <a href='#' className='card-link'>
                             Posted by <UserHeader author_id={author}/>
                         </a>
