4b0e5d444652f6003ccc438a26aa64759e7d53e7
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-10 14:43:13 +0530

    reusable modal

--- frontend/src/components/Blogs/BlogDelete.js
+++ frontend/src/components/Blogs/BlogDelete.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import '../styles/Blogs/Blog.css'
 import Modal from '../Modal'
@@ -8,23 +8,30 @@ import history from '../../history'
 
 const BlogDelete = (props) => {
 
+    const actions = (
+        // to avoid an extra div
+        <>
+            <button type="button" style={{backgroundColor:'#d63447'}} class="btn">Delete</button>
+            <button type="button" style={{backgroundColor:'grey'}} class="btn">Cancel</button>        
+        </>
+    )
+
     return (
         <div>
             <h2>Stream Delete</h2>
-            {/* <Modal header='Delete Stream'
-                    content={renderContent()}
-                    action= {renderAction()}
-                    onDismiss={() => history.push('/')}
-            /> */}
-            <Modal />
+            <Modal 
+                header='Delete Blog'
+                content='Are you sure you want to delete this blog?'
+                actions={actions}
+                onDismiss={() => history.push('/')}
+            />
         </div>
     )
 }
 
 
-// const mapStateToProps = (state, ownProps) => {
-//     return { blog: state.blogs[ownProps.match.params.id]}
-// }
+const mapStateToProps = (state, ownProps) => {
+    return { blog: state.blogs[ownProps.match.params.id]}
+}
 
-// export default connect(mapStateToProps, {BlogDelete})(BlogDelete)
-export default BlogDelete
+export default connect(mapStateToProps, {deleteBlog})(BlogDelete)
--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -4,37 +4,25 @@ import history from '../history'
 
 import './styles/Modal.css'
 
-const Modal = (props) => {
+const Modal = ({header, content, actions, onDismiss}) => {
     return createPortal(
-        <div className='modal-container'
-            onClick={()=> history.push('/')}>
-            {/* <div className='ui modal active'  
-                 onClick={(e) => e.stopPropagation()}
-                 style={inlineModalStyles}>
-            <div className="header">{props.header}</div>
-                <div className="content">
-                    {props.content}
-                </div>
-                <div className='actions'>
-                    {props.action}
-                </div>
-                
-            </div> */}
+        <div 
+            className='modal-container'
+            onClick={()=> onDismiss()}
+            >
             <div 
                 className='modal-content'
                 onClick={(e) => e.stopPropagation()}>
-                    
+
                 <div className='modal-header'>
-                    <h4 class="modal-title">Delete Blog</h4>
+                    <h4 class="modal-title">{header}</h4>
                 </div>
                 <div class="modal-body">
-                    <p>Are you sure you want to delete this blog?</p>
+                    <p>{content}</p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" style={{backgroundColor:'#d63447'}} class="btn">Delete</button>
-                    <button type="button" style={{backgroundColor:'grey'}} class="btn">Cancel</button>
+                    {actions}
                 </div>
-
             </div>
 
         </div>,
