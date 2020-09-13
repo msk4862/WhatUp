c7e968eb82beafc14a31910da31a1ccdee24a66a
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-09-13 16:30:52 +0530

    schema updates

--- backend-firebase/dbSchema.js
+++ backend-firebase/dbSchema.js
@@ -2,7 +2,7 @@ let db = {
     posts: [
         {
             userHandle: "user",
-            userImage: "",
+            imageUrl: "",
             bodyMeta: "this is body meta",
             body: "this is body",
             createdAt: "2020-09-08T11:04:03.724Z", //ISO string
