fc3ff757d4da63303c89b14304e3ed8e5b62d11e
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-09-15 18:42:48 +0530

    firebase api used

--- frontend/src/apis/FirebaseAPI.js
+++ frontend/src/apis/FirebaseAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://us-central1-what-up-bde10.cloudfunctions.net/api",
+    baseURL: "https://us-central1-what-up-bde10.cloudfunctions.net/api"
 })
