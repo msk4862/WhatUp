918845ac9721c0a6b9673065988e4f15018641cb
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-09-24 20:44:59 +0530

    userTile comp

---
+++ frontend/src/components/UserTile.js
@@ -0,0 +1,17 @@
+import React from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "../styles/Blogs/userTile.css";
+
+const UserTile = ({ userImage, userHandle, createdAt }) => {
+    dayjs.extend(relativeTime);
+
+    return (
+        <div className="user-tile">
+            <img src={userImage} alt={`${userHandle}`}/>
+            <span><a href="#">{userHandle}</a>{dayjs(createdAt).fromNow()}</span>
+        </div>
+    );
+}
+
+export default UserTile;
