d8bb14aae3507fe9ef18f50035a9e7f238e3081a
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-09-28 00:19:57 +0530

    cardskeleton improvement

--- frontend/src/components/Skeletons/CardSkeleton.js
+++ frontend/src/components/Skeletons/CardSkeleton.js
@@ -2,24 +2,25 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 
 const CardSkeleton = () => (
-    <div>
+    <>
         {Array(3)
             .fill()
             .map((item, index) => (
                 <div key={index} className="card">
                     <div className="card-body">
                         <h5 className="card-title">
-                            <Skeleton height={40} />
+                            <Skeleton height={30} width={`80%`} />
                         </h5>
                         <p className="card-text">
                             <Skeleton count={2} />
+                            <Skeleton width={`50%`} />
                         </p>
                         <div className="row meta-data align-items-center">
                             <div className="col-12 col-sm-6">
                                 <Skeleton
                                     circle={true}
-                                    height={30}
-                                    width={30}
+                                    height={50}
+                                    width={50}
                                 />
                             </div>
                             <div className="col-12 col-sm-6">
@@ -29,7 +30,7 @@ const CardSkeleton = () => (
                     </div>
                 </div>
             ))}
-    </div>
+    </>
 );
 
 export default CardSkeleton;
