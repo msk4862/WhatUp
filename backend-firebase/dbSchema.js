a25bdc51c7ff96cfbfd36914937a91dd4b60fe16
msk4862 <msk4862@gmail.com>
2020-11-01 16:03:47 +0530

    post image scheme reverted

--- backend-firebase/dbSchema.js
+++ backend-firebase/dbSchema.js
@@ -9,7 +9,6 @@ let db = {
             createdAt: "2020-09-08T11:04:03.724Z", //ISO string
             likeCount: 2,
             commentsCount: 4,
-            postImage: "",
         },
     ],
 
